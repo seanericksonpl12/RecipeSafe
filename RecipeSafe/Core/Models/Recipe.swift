336e7c691756459ac9763c1ebab0ce58c1f7d8c0
seanericksonpl12 <seanericksonpl12@gmail.com>
2023-07-22 11:19:27 -0500

    Moved recipe construction from json to recipe model init and updated RecipeView Instructions list

--- RecipeSafe/Core/Models/Recipe.swift
+++ RecipeSafe/Core/Models/Recipe.swift
@@ -6,9 +6,11 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 struct Recipe: Hashable {
     
+    // MARK: - Core Data Init
     init(dataItem: RecipeItem) {
         self.id = dataItem.id ?? UUID()
         self.title = dataItem.title ?? "error"
@@ -21,11 +23,40 @@ struct Recipe: Hashable {
         self.url = dataItem.url
     }
     
+    // MARK: - Testing Init
     init(id: UUID = UUID(), title: String, ingredients: [String], img: URL? = nil) {
         self.id = id
         self.title = title
         self.ingredients = ingredients
         self.img = img
+    }
+    
+    // MARK: - JSON Init
+    init?(json: JSON) {
+        let info = json[JSONKeys.allInfo.rawValue]
+        if info.isEmpty || !info.exists() { return nil }
+        
+        let title: String = info[0][JSONKeys.title.rawValue].stringValue
+        let ingrd: [String] = info[7].filter { $0.0.contains(JSONKeys.ingredient.rawValue)}[0].1.arrayValue.map { $0.stringValue }
+        let img: URL? = info[0][JSONKeys.imageUrl.rawValue].url
+        let description: String = info[1][JSONKeys.description.rawValue].stringValue
+        let instructions: [String] = info[7][JSONKeys.instructions.rawValue].arrayValue.map { $0[JSONKeys.instructionValue.rawValue].stringValue }
+        let prep = info[7][JSONKeys.prepTime.rawValue].stringValue
+        let cook = info[7][JSONKeys.cookTime.rawValue].stringValue
+        let url = info[7][JSONKeys.url.rawValue].url
+        
+        if title == "" || ingrd.isEmpty || instructions.isEmpty || url == nil || url?.absoluteString == "" {
+            return nil
+        }
+        
+        self.title = title
+        self.ingredients = ingrd
+        self.img = img
+        self.description = description
+        self.instructions = instructions
+        self.prepTime = prep
+        self.cookTime = cook
+        self.url = url
     }
     
     var id: UUID = UUID()
@@ -39,14 +70,15 @@ struct Recipe: Hashable {
     var cookTime: String?
 }
 
-struct RecipeData: Codable {
-    
-    let context: String
-    let graph: [[String:String]]
-    
-    
-    enum CodingKeys: String, CodingKey {
-        case graph = "@graph"
-        case context = "@context"
-    }
+fileprivate enum JSONKeys: String {
+    case allInfo = "@graph"
+    case description
+    case title = "headline"
+    case ingredient = "recipeIngredient"
+    case instructions = "recipeInstructions"
+    case instructionValue = "text"
+    case imageUrl = "thumbnailUrl"
+    case prepTime
+    case cookTime
+    case url
 }
--- RecipeSafe/Core/Network/NetworkManager.swift
+++ RecipeSafe/Core/Network/NetworkManager.swift
@@ -13,6 +13,7 @@ import SwiftyJSON
 final class NetworkManager {
     
     static let main: NetworkManager = NetworkManager()
+    private let scriptTag: String = "script[type=application/ld+json]"
     
     func networkRequest(url: String) -> AnyPublisher<Recipe?, Error> {
         let slicedURL = url.replacing("RecipeSafe://", with: "")
@@ -41,59 +42,23 @@ final class NetworkManager {
     
     func soupify(html: String) throws -> Recipe? {
         let doc: Document = try SwiftSoup.parse(html)
-        let scripts = try doc.select(JSONKeys.scriptTag.rawValue).first()?.data()
+        let scripts = try doc.select(scriptTag).first()?.data()
         guard let jsonString = scripts?.data(using: .utf8, allowLossyConversion: false) else { return nil }
-        
-        
         let json = try JSON(data: jsonString)
-        let info = json[JSONKeys.allInfo.rawValue]
-        if info.isEmpty || !info.exists() { return nil }
-        
-        let title: String = info[0][JSONKeys.title.rawValue].stringValue
-        let ingrd: [String] = info[7].filter { $0.0.contains(JSONKeys.ingredient.rawValue)}[0].1.arrayValue.map { $0.stringValue }
-        let img: URL? = info[0][JSONKeys.imageUrl.rawValue].url
-        let description: String = info[1][JSONKeys.description.rawValue].stringValue
-        let instructions: [String] = info[7][JSONKeys.instructions.rawValue].arrayValue.map { $0[JSONKeys.instructionValue.rawValue].stringValue }
-        let prep = info[7][JSONKeys.prepTime.rawValue].stringValue
-        let cook = info[7][JSONKeys.cookTime.rawValue].stringValue
-        let url = info[7][JSONKeys.url.rawValue].url
-        
-        var recipe = Recipe(title: title, ingredients: ingrd, img: img)
-        recipe.description = description
-        recipe.instructions = instructions
-        recipe.prepTime = prep
-        recipe.cookTime = cook
-        recipe.url = url
-        
-        
-//        print("instructions: \(instructions)")
-//
-//
-//        print("count: \(info.count)")
-//        print("0: \(info[0])")
-//        print("1: \(info[1])")
-//        print("2: \(info[2])")
-//        print("3: \(info[3])")
-//        print("4: \(info[4])")
-//        print("5: \(info[5])")
-//        print("6: \(info[6])")
-//        print("7: \(info[7])")
-        
-        return recipe
-        
+        return Recipe(json: json)
     }
     
-    private enum JSONKeys: String {
-        case scriptTag = "script[type=application/ld+json]"
-        case allInfo = "@graph"
-        case description
-        case title = "headline"
-        case ingredient = "recipeIngredient"
-        case instructions = "recipeInstructions"
-        case instructionValue = "text"
-        case imageUrl = "thumbnailUrl"
-        case prepTime
-        case cookTime
-        case url
+    // MARK: - TESTING ONLY
+    private func printJSON(json: JSON) {
+        let info = json["@graph"]
+        print("count: \(info.count)")
+        print("0: \(info[0])")
+        print("1: \(info[1])")
+        print("2: \(info[2])")
+        print("3: \(info[3])")
+        print("4: \(info[4])")
+        print("5: \(info[5])")
+        print("6: \(info[6])")
+        print("7: \(info[7])")
     }
 }
